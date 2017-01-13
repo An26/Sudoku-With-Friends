eefbe865ce810e8f5c345f2fc89e8efbc5dcf6c5
An Huynh <An Huynh>
2017-01-12 23:50:37 -0600

    adding the sudoku game board

--- app/components/PlayGame.js
+++ app/components/PlayGame.js
@@ -6,6 +6,113 @@ var PlayGame = React.createClass({
 			<div>
 				<p>This is the place where users actually play sudoku either by themselves or with someone they know...
 				</p>
+
+				<table>
+  <tr>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+  </tr>
+  <tr>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+  </tr>
+  <tr>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+  </tr>
+  <tr>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+  </tr>
+  <tr>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+  </tr>
+  <tr>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+  </tr>
+  <tr>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+  </tr>
+  <tr>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+  </tr>
+  <tr>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+    <td> x </td>
+  </tr>
+</table>
+
+
+
+
+
 			</div>
 			)
 	}
