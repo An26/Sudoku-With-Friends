d65ca25d295099a8b984900d518d1a8d5a9a001b
Stella Wu <stellawu@utexas.edu>
2017-01-28 12:42:03 -0600

    adding validation rules and allow input on game board

--- app/pages/Game.js
+++ app/pages/Game.js
@@ -5,14 +5,12 @@ import { connect } from 'react-redux';
 import  Chat  from '../Chat/Chat';
 
 function printboard(board) {
-	var out = '';
+	var out = [];
 
 	for (var row = 0; row < 9; row++) {
 		for (var col = 0; col < 9; col++) {
-			out += [""," "," ","  "," "," ","  "," "," "][col];
-      		out += printcode(board[sudoku.posfor(row, col)]);
+      		out.push(printcode(board[sudoku.posfor(row, col)]));
 		}
-		out += ['\n','\n','\n\n','\n','\n','\n\n','\n','\n','\n'][row];
 	}
 
 	return out;
@@ -20,7 +18,7 @@ function printboard(board) {
 
 function printcode(n) {
 	if (n == null) {
-		return '_';
+		return '';
 	}
 
 	return n + 1 + '';
@@ -52,6 +50,9 @@ export default class Game extends React.Component {
 	constructor(props, context) {
 		super(props, context);
 		// console.log('props', props);
+		this.state = {
+			puzzle: printboard(puzzle)
+		}
 	}
 
 
@@ -74,6 +75,42 @@ export default class Game extends React.Component {
 		return;
 	}
 
+	handleInput(event) {
+        let newValue = event.target.value;
+        if (newValue===1 || newValue===2 ||newValue===3 ||newValue===4
+        	||newValue===5 || newValue===6 ||newValue===7 ||newValue===8
+        	||newValue===9) {
+	        let cellIndex = event.target.id;
+	        let copy = this.state.puzzle;
+	        copy[cellIndex] = newValue;
+	        this.setState({puzzle: copy});
+    	} else {
+    		event.preventDefault();
+    	}
+    }
+
+    generateCells(rowNumber) {
+    	var rows = [];
+    	for (var i = rowNumber*9; i < rowNumber*9+9; i++) {
+    		if(this.state.puzzle[i]==="") {
+    			rows.push(<td key={i}><input id={i} onChange={this.handleInput.bind(this)} value={this.state.puzzle[i]} placeholder="_" className="cell" type="integer" maxLength="1" min="1" max="9"/></td>)
+    		} else {
+    		rows.push(<td key={i} id={i}>{this.state.puzzle[i]}</td>)
+    		}
+    	}
+    	return rows;
+    }
+
+    generateGame() {
+    	var board=[];
+    	for (var i = 0; i < 9; i++) {
+    		board.push(<tr key={i}>
+    			{this.generateCells(i)}
+    		</tr>)
+    	}
+    	return board;
+    }
+
 	render (){
 		return (
 		<div>
@@ -92,105 +129,7 @@ export default class Game extends React.Component {
 				<div>
 				<table>
 				<tbody>
-					<tr>
-					    <td> {printboard(puzzle)[0]} </td>
-					    <td> {printboard(puzzle)[2]} </td>
-					    <td> {printboard(puzzle)[4]} </td>
-					    <td> {printboard(puzzle)[7]} </td>
-					    <td> {printboard(puzzle)[9]} </td>
-					    <td> {printboard(puzzle)[11]} </td>
-					    <td> {printboard(puzzle)[14]} </td>
-					    <td> {printboard(puzzle)[16]} </td>
-					    <td> {printboard(puzzle)[18]} </td>
-					  </tr>
-					<tr>
-					    <td> {printboard(puzzle)[20]} </td>
-					    <td> {printboard(puzzle)[22]} </td>
-					    <td> {printboard(puzzle)[24]} </td>
-					    <td> {printboard(puzzle)[27]} </td>
-					    <td> {printboard(puzzle)[29]} </td>
-					    <td> {printboard(puzzle)[31]} </td>
-					    <td> {printboard(puzzle)[34]} </td>
-					    <td> {printboard(puzzle)[36]} </td>
-					    <td> {printboard(puzzle)[38]} </td>
-					  </tr>
-					  <tr>
-					    <td> {printboard(puzzle)[40]} </td>
-					    <td> {printboard(puzzle)[42]} </td>
-					    <td> {printboard(puzzle)[44]} </td>
-					    <td> {printboard(puzzle)[47]} </td>
-					    <td> {printboard(puzzle)[49]} </td>
-					    <td> {printboard(puzzle)[51]} </td>
-					    <td> {printboard(puzzle)[54]} </td>
-					    <td> {printboard(puzzle)[56]} </td>
-					    <td> {printboard(puzzle)[58]} </td>
-					  </tr>
-					  <tr>
-					    <td> {printboard(puzzle)[61]} </td>
-					    <td> {printboard(puzzle)[63]} </td>
-					    <td> {printboard(puzzle)[65]} </td>
-					    <td> {printboard(puzzle)[68]} </td>
-					    <td> {printboard(puzzle)[70]} </td>
-					    <td> {printboard(puzzle)[72]} </td>
-					    <td> {printboard(puzzle)[75]} </td>
-					    <td> {printboard(puzzle)[77]} </td>
-					    <td> {printboard(puzzle)[79]} </td>
-					  </tr>
-					  <tr>
-					    <td> {printboard(puzzle)[81]} </td>
-					    <td> {printboard(puzzle)[83]} </td>
-					    <td> {printboard(puzzle)[85]} </td>
-					    <td> {printboard(puzzle)[88]} </td>
-					    <td> {printboard(puzzle)[90]} </td>
-					    <td> {printboard(puzzle)[92]} </td>
-					    <td> {printboard(puzzle)[95]} </td>
-					    <td> {printboard(puzzle)[97]} </td>
-					    <td> {printboard(puzzle)[99]} </td>
-					  </tr>
-					  <tr>
-					    <td> {printboard(puzzle)[101]} </td>
-					    <td> {printboard(puzzle)[103]} </td>
-					    <td> {printboard(puzzle)[105]} </td>
-					    <td> {printboard(puzzle)[108]} </td>
-					    <td> {printboard(puzzle)[110]} </td>
-					    <td> {printboard(puzzle)[112]} </td>
-					    <td> {printboard(puzzle)[115]} </td>
-					    <td> {printboard(puzzle)[117]} </td>
-					    <td> {printboard(puzzle)[119]} </td>
-					  </tr>
-					  <tr>
-					    <td> {printboard(puzzle)[122]} </td>
-					    <td> {printboard(puzzle)[124]} </td>
-					    <td> {printboard(puzzle)[126]} </td>
-					    <td> {printboard(puzzle)[129]} </td>
-					    <td> {printboard(puzzle)[131]} </td>
-					    <td> {printboard(puzzle)[133]} </td>
-					    <td> {printboard(puzzle)[136]} </td>
-					    <td> {printboard(puzzle)[138]} </td>
-					    <td> {printboard(puzzle)[140]} </td>
-					  </tr>
-					  <tr>
-					    <td> {printboard(puzzle)[142]} </td>
-					    <td> {printboard(puzzle)[144]} </td>
-					    <td> {printboard(puzzle)[146]} </td>
-					    <td> {printboard(puzzle)[149]} </td>
-					    <td> {printboard(puzzle)[151]} </td>
-					    <td> {printboard(puzzle)[153]} </td>
-					    <td> {printboard(puzzle)[156]} </td>
-					    <td> {printboard(puzzle)[158]} </td>
-					    <td> {printboard(puzzle)[160]} </td>
-					  </tr>
-					 <tr>
-					    <td> {printboard(puzzle)[162]} </td>
-					    <td> {printboard(puzzle)[164]} </td>
-					    <td> {printboard(puzzle)[166]} </td>
-					    <td> {printboard(puzzle)[169]} </td>
-					    <td> {printboard(puzzle)[171]} </td>
-					    <td> {printboard(puzzle)[173]} </td>
-					    <td> {printboard(puzzle)[176]} </td>
-					    <td> {printboard(puzzle)[178]} </td>
-					    <td> {printboard(puzzle)[180]} </td>
-					  </tr>
+					{this.generateGame()}
 				</tbody>
 				</table>
 			</div>
