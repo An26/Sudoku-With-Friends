9cdfa98e171469f1771903f83e6c62209c5e74f3
An Huynh <An Huynh>
2017-02-04 12:50:05 -0600

    adding divs for playGame Multiplayer

--- app/components/Game/Pages/PlayGame.js
+++ app/components/Game/Pages/PlayGame.js
@@ -6,110 +6,16 @@ export default class PlayGame extends React.Component {
 			<div>
 				<p>This is the place where users actually play sudoku either by themselves or with someone they know...
 				</p>
+				<div>chat box here on the side</div>
+				<div>game level/difficulty</div>
+				<div>timer</div>
+				<div>main gaame board</div>
+				<div>number options for input</div>
+				<div>opponent's name</div>
+				<div>opponent's game board</div>
+				<div>Start Game Btn</div>
+				<div>End Game/ Quit btn</div>
 
-				<table>
-					<tbody>
-					  <tr>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					  </tr>
-					  <tr>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					  </tr>
-					  <tr>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					  </tr>
-					  <tr>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					  </tr>
-					  <tr>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					  </tr>
-					  <tr>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					  </tr>
-					  <tr>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					  </tr>
-					  <tr>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					  </tr>
-					  <tr>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					    <td> x </td>
-					  </tr>
-				  </tbody>
-				</table>
 			</div>
 			)
 	}
